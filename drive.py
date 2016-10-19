f207b4b02bdca987c5c11fcf5abaf3ceea63b420
dsilver829@gmail.com <dsilver829@gmail.com>
2016-10-19 18:14:01 +0000

    train a new model

---
+++ drive.py
@@ -0,0 +1,69 @@
+import argparse
+import base64
+import json
+
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+import time
+from PIL import Image
+from PIL import ImageOps
+from flask import Flask, render_template
+from io import BytesIO
+
+from keras.models import model_from_json
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+
+
+sio = socketio.Server()
+app = Flask(__name__)
+model = None
+prev_image_array = None
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    # Parse the Data and Emit a New Steering Command
+    steering_angle = data["steering_angle"]
+    throttle = data["throttle"]
+    speed = data["speed"]
+    imgString = data["image"]
+    image = Image.open(BytesIO(base64.b64decode(imgString)))
+    image_array = np.asarray(image)
+    transformed_image_array = image_array[None, :, :, :]
+    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    throttle = 1/(1 + abs(steering_angle)) * 0.2
+    print(steering_angle, throttle)
+    send_control(steering_angle/25., throttle)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit("steer", data={
+    'steering_angle': steering_angle.__str__(),
+    'throttle': throttle.__str__()
+    }, skip_sid=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remote Driving')
+    parser.add_argument('model', type=str,
+    help='Path to model definition json. Model weights should be on the same path.')
+    args = parser.parse_args()
+    with open(args.model, 'r') as jfile:
+        model = model_from_json(json.load(jfile))
+
+    model.compile("adam", "mse")
+    weights_file = args.model.replace('json', 'h5')
+    model.load_weights(weights_file)
+
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
---
+++ model.py
@@ -0,0 +1,91 @@
+import os
+import argparse
+import json
+import csv
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
+from keras.layers.convolutional import Convolution2D
+from scipy import misc
+import numpy as np
+
+def get_model(time_len=1):
+	row, col, ch = 80, 320, 3
+
+	model = Sequential()
+	model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
+	model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+	model.add(ELU())
+	model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+	model.add(ELU())
+	model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+	model.add(Flatten())
+	model.add(Dropout(.2))
+	model.add(ELU())
+	model.add(Dense(512))
+	model.add(Dropout(.5))
+	model.add(ELU())
+	model.add(Dense(1))
+
+	model.compile(optimizer="adam", loss="mse")
+
+	return model
+
+#def get_data():
+def BatchGenerator():
+	X = []
+	y = []
+
+	with open('driving_log.csv', 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+		i = 0
+		for row in reader:
+			i += 1
+			if(i % 10000 == 0):
+				yield np.asarray(X), np.asarray(y)
+				X = []
+				y = []
+
+			if("RECORDING" in row[0]):
+				continue
+			img_center_filename = row[0]
+			img_left_filename = row[1]
+			img_right_filename = row[2]
+			
+			img_center = misc.imread(img_center_filename)[:,:,:]
+			img_left = misc.imread(img_left_filename)[:,:,:]
+			img_right = misc.imread(img_right_filename)[:,:,:]
+	
+			steering_angle = float(row[3])
+			steering_angle_left = steering_angle + abs(steering_angle)*0.3
+			steering_angle_right = steering_angle - abs(steering_angle)*0.3
+		
+			num_samples = 1
+			if(steering_angle > 0):
+				num_samples = 3	
+
+			for j in range(num_samples):
+				X.append(img_center)
+				y.append(steering_angle)
+				X.append(img_left)
+				y.append(steering_angle_left)
+				X.append(img_right)
+				y.append(steering_angle_right)
+
+	yield np.asarray(X), np.asarray(y)
+
+if __name__ == "__main__":
+	#print("MAIN")
+	#X, y = get_data()
+	#print(X.shape)
+	#print(y.shape)
+	model = get_model()
+	#X = np.reshape(X, (X.shape[0], 3, 160, 320))
+	#print("FIT")
+	for i in (range(3)):
+		print("Epoch: %d\n" % i)
+		for X, y in BatchGenerator():
+			model.fit(X, y, batch_size=32, nb_epoch=1)
+	#model.save('./comma.h5')
+	model.save_weights("./model.keras")
+	with open('./model.json', 'w') as outfile:
+		json.dump(model.to_json(), outfile)
